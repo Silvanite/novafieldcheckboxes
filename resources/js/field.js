e4921cb0ccc5d05245a532a3ce41be91726d0643
Marco Mark <marco@silvanite.com>
2018-09-01 15:53:26 +0100

    Publish compiled production assets

--- resources/js/field.js
+++ resources/js/field.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 Vue.config.devtools = true
 
 Nova.booting((Vue, router) => {
-    Vue.component('index-nova-field-checkboxes', require('./components/IndexField'));
-    Vue.component('detail-nova-field-checkboxes', require('./components/DetailField'));
-    Vue.component('form-nova-field-checkboxes', require('./components/FormField'));
+    Vue.component('index-novafieldcheckboxes', require('./components/IndexField'));
+    Vue.component('detail-novafieldcheckboxes', require('./components/DetailField'));
+    Vue.component('form-novafieldcheckboxes', require('./components/FormField'));
 })
